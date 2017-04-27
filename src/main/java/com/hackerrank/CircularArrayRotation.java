8fd88eb0b88cb8a2e59a4332175921d4a46d7582
Rajesh Kumar <rajeshkumar@xebia.com>
2017-04-28 01:13:58 +0530

    Circular Array Rotation

---
+++ src/main/java/com/hackerrank/CircularArrayRotation.java
@@ -0,0 +1,40 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * Created by rajeshkumar on 27/04/17.
+ */
+public class CircularArrayRotation {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int q = in.nextInt();
+
+        int[] array = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            array[a_i] = in.nextInt();
+        }
+
+        int[] indexArray = new int[q];
+        for (int a0 = 0; a0 < q; a0++) {
+            indexArray[a0] = in.nextInt();
+        }
+
+        int[] rotatedArray = rotateArray(array, k);
+        for (int indexValue : indexArray) {
+            System.out.println(rotatedArray[indexValue]);
+        }
+    }
+
+    private static int[] rotateArray(final int[] array, final int k) {
+        int[] rotatedArray = new int[array.length];
+        for (int counter = 0; counter < array.length; counter++) {
+            int rotatedIndex = counter + k < array.length ? counter + k : (counter + k) % array.length;
+            rotatedArray[rotatedIndex] = array[counter];
+        }
+        return rotatedArray;
+    }
+}
---
+++ src/main/java/com/hackerrank/ViralAdvertising.java
@@ -0,0 +1,27 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * Created by rajeshkumar on 27/04/17.
+ */
+public class ViralAdvertising {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int likePeople = findPeople(n);
+        System.out.println(likePeople);
+    }
+
+    private static int findPeople(final int n) {
+        int sendPeople = 5;
+        int likePeople = 0;
+        for (int counter = 1; counter <= n; counter++) {
+            int floor = sendPeople / 2;
+            likePeople += floor;
+            sendPeople = floor * 3;
+        }
+        return likePeople;
+    }
+}
