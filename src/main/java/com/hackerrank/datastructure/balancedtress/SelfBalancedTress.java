f0b353d94ad60c76f00a72a47675642544d90442
Rajesh Kumar <rajeshkumar@xebia.com>
2017-05-20 21:28:59 +0530

    Self balancing trees

---
+++ src/main/java/com/hackerrank/datastructure/balancedtress/SelfBalancedTress.java
@@ -0,0 +1,91 @@
+package com.hackerrank.datastructure.balancedtress;
+
+/**
+ * Created by rajeshkumar on 20/05/17.
+ */
+public class SelfBalancedTress {
+    public static void main(String[] args) {
+
+    }
+
+    static Node insert(Node node, int key) {
+        if (node == null) {
+            Node newNode = new Node();
+            newNode.val = key;
+            newNode.ht = 0;
+            return newNode;
+        }
+        if (key < node.val) {
+            node.left = insert(node.left, key);
+        } else if (key > node.val) {
+            node.right = insert(node.right, key);
+        } else {
+            return node;
+        }
+
+        node.ht = height(node.left) > height(node.right) ? height(node.left) + 1 : height(node.right) + 1;
+
+        final int balance = getBalance(node);
+
+        if (balance > 1 && key < node.left.val) {
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && key > node.right.val) {
+            return rotateLeft(node);
+        }
+
+        if (balance > 1 && key > node.left.val) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && key < node.right.val) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    static Node rotateRight(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.ht = height(y.left) > height(y.right) ? height(y.left) + 1 : height(y.right) + 1;
+        x.ht = height(x.left) > height(x.right) ? height(x.left) + 1 : height(x.right) + 1;
+        return x;
+    }
+
+    static Node rotateLeft(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.ht = height(x.left) > height(x.right) ? height(x.left) + 1 : height(x.right) + 1;
+        y.ht = height(y.left) > height(y.right) ? height(y.left) + 1 : height(y.right) + 1;
+
+        return y;
+    }
+
+    static int getBalance(Node node) {
+        return node != null ? height(node.left) - height(node.right) : 0;
+    }
+
+    static int height(Node node) {
+        return node != null ? node.ht : -1;
+    }
+
+}
+
+class Node {
+    int val;   //Value
+    int ht;      //Height
+    Node left;   //Left child
+    Node right;   //Right child
+}
--- src/main/java/com/hackerrank/datastructure/disjointsets/KunduAndTree.java
+++ src/main/java/com/hackerrank/datastructure/disjointsets/KunduAndTree.java
@@ -1,7 +1,31 @@
 package com.hackerrank.datastructure.disjointsets;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by rajeshkumar on 20/05/17.
  */
 public class KunduAndTree {
+    static final int M = 1000000007;
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Vertex {
+        private ArrayList<Edge> neighborhood;
+
+    }
+
+    static class Edge {
+        private Vertex one, two;
+        private int weight;
+
+    }
+
+    static class Graph {
+        private HashMap<String, Vertex> vertices;
+        private HashMap<Integer, Edge> edges;
+    }
 }
